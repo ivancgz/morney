b1dc4c1a1b6cd4dc9eb470cca8cade4217646a92
ivan <ivancgz@163.com>
2021-10-12 11:44:44 +0800

    导航放到底部

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,11 +1,14 @@
 <template>
-$END$
+  <div>
+    <div>当前页面不存在,请重新检查网址路径</div>
+    <div><router-link to="/">返回首页</router-link></div>
+  </div>
 </template>
 
 <script lang="ts">
 export default {
-name: "NotFound"
-}
+  name: 'NotFound'
+};
 </script>
 
 <style lang="scss" scoped>
