7c95ce191490b6a5274bca0e84b7ade3bd8600dd
ivan <ivancgz@163.com>
2021-11-30 17:32:47 +0800

    抽离 Tabs.vue, 使用在 Statistics.vue 里面

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,19 +1,20 @@
 <template>
   <div>
     <ul class="types">
-      <li :class="value === '-' && 'selected'" @click="selectType('-')">支出</li>
-      <li :class="value === '+' && 'selected'" @click="selectType('+')">收入</li>
+      <li :class="{[classPrefix + '-item']: classPrefix, selected: value === '-'}" @click="selectType('-')">支出</li>
+      <li :class="{[classPrefix + '-item']: classPrefix, selected: value === '+'}" @click="selectType('+')">收入</li>
     </ul>
   </div>
 </template>
 
 <script lang="ts">
   import Vue from 'vue'
-  import {Component, Prop, Watch} from 'vue-property-decorator';
+  import {Component, Prop} from 'vue-property-decorator';
 
   @Component
   export default class Types extends Vue{
-    @Prop() readonly value!: string;
+    @Prop(String) readonly value!: string;
+    @Prop(String) classPrefix?: string;
 
     // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
     selectType(type: string){
